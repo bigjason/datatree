d3375e1e9e8e7282537441888a133e513d8e8983
Jason Webb <bigjasonwebb@gmail.com>
2011-05-31 16:59:47 -0600

    Fix whitespace and quotes.

--- datatree/tests/test_etreerender.py
+++ datatree/tests/test_etreerender.py
@@ -7,45 +7,34 @@ except ImportError:
 from datatree.node import Node
 
 class test_ETreeRenderer(unittest.TestCase):
-
-
-    def setUp(self):
-        pass
-
-
-    def tearDown(self):
-        pass
-
-
     def test_single_level(self):
-        root = Node("a", "Here", href="url", title="A Title")
-        actual = root.render("xml")
+        root = Node('a', 'Here', href='url', title='A Title')
+        actual = root.render('xml')
         result = e.fromstring(actual)
 
-        self.assertEqual(result.tag, "a")
-        self.assertEqual(result.attrib["href"], "url")
-        self.assertEqual(result.attrib["title"], "A Title")
-        self.assertEqual(result.text, "Here")
+        self.assertEqual(result.tag, 'a')
+        self.assertEqual(result.attrib['href'], 'url')
+        self.assertEqual(result.attrib['title'], 'A Title')
+        self.assertEqual(result.text, 'Here')
 
     def test_multi_level(self):
-        root = Node("root", level=1)
+        root = Node('root', level=1)
         with root.second(level=2) as two:
-            two.third("Three", level=3)
-        root.fourth("four", level=2)
+            two.third('Three', level=3)
+        root.fourth('four', level=2)
 
-        actual = root.render("xml")
+        actual = root.render('xml')
 
         # Parse the xml and break it into pieces to assert.
         result = e.fromstring(actual)
-        second = result.find("second")
-        third = result.find(".//third")
-        fourth = result.find("fourth")
+        second = result.find('second')
+        third = result.find('.//third')
+        fourth = result.find('fourth')
 
-        self.assertEqual(result.tag, "root")
-        self.assertEqual(result.attrib["level"], "1")
-        self.assertEqual(second.attrib["level"], "2")
-        self.assertEqual(third.text, "Three")
-        self.assertEqual(third.attrib["level"], "3")
-        self.assertEqual(fourth.text, "four")
-        self.assertEqual(fourth.attrib["level"], "2")
-
+        self.assertEqual(result.tag, 'root')
+        self.assertEqual(result.attrib['level'], '1')
+        self.assertEqual(second.attrib['level'], '2')
+        self.assertEqual(third.text, 'Three')
+        self.assertEqual(third.attrib['level'], '3')
+        self.assertEqual(fourth.text, 'four')
+        self.assertEqual(fourth.attrib['level'], '2')
